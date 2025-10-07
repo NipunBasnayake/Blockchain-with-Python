79b65ff3a811f38157c4a7a40ff9250ee73e6bd3
NipunBasnayake <nipunsathsara1999@gmail.com>
2025-10-07 23:50:09 +0530

    print initial block data

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import hashlib
 
+
 class TestCoinBlock:
     def __init__(self, previouse_block_hash, transaction_list):
         self.previouse_block_hash = previouse_block_hash
@@ -7,4 +8,15 @@ class TestCoinBlock:
         self.block_data = "-".join(transaction_list) + "-" + previouse_block_hash
         self.block_hash = hashlib.sha256(self.block_data.encode()).hexdigest()
 
-        
+
+t1 = "Nipun sends 2.1 TC to Alice"
+t2 = "Bob sends 4.1 TC to Alice"
+t3 = "Alice sends 1.2 TC to Bob"
+t4 = "Bob sends 3.2 TC to Nipun"
+t5 = "Alice sends 2.3 TC to Nipun"
+t6 = "Nipun sends 1.5 TC to Bob"
+
+initial_block = TestCoinBlock("Initial String", [t1, t2])
+print(initial_block.block_data)
+print(initial_block.block_hash)
+
