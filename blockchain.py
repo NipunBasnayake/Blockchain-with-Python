49228fe81489243832d64d3f835f29f743088732
NipunBasnayake <nipunsathsara1999@gmail.com>
2025-10-08 23:17:01 +0530

    new block

--- blockchain.py
+++ blockchain.py
@@ -9,4 +9,16 @@ class Blockchain(object):
     
         self.new_block(previous_hash='Haha Blockchain Learinig is really cool',proof=123)
 
+    def new_block(self,proof,previous_hash=None):
+        blobk = {
+            'index':len(self.chain)+1,
+            'timestamp':time(),
+            'transactions':self.pending_transactions,
+            'proof':proof,
+            'previous_hash':previous_hash or self.hash(self.chain[-1])
+        }
+        self.pending_transactions=[]
+        self.chain.append(block)
+        return block
+    
     
