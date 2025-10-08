49b20add9ddfbe376a45b067a63a54cada9a8f17
NipunBasnayake <nipunsathsara1999@gmail.com>
2025-10-08 23:15:08 +0530

    Start Implementing Simple Bitcoin Blockchain

---
+++ blockchain.py
@@ -0,0 +1,12 @@
+import hashlib
+import json
+from time import time
+
+class Blockchain(object):
+    def __init__:
+        self.chain=[]
+        self.penfing_transactions=[]
+    
+        self.new_block(previous_hash='Haha Blockchain Learinig is really cool',proof=123)
+
+    
